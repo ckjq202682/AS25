804c8ab56d02e2b3f0adbde8d3fd168099fd9082
ckjq202682 <63698151+ckjq202682@users.noreply.github.com>
2020-10-12 17:39:22 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+#AS25
+
+num = []
+
+for i in range(3):
+    x = int(input("add value"))
+    num.append(x)
+
+def histogram():
+    print("*" * num[0])
+    print("*" * num[1])
+    print("*" * num[2])
+
+histogram()
+
+
